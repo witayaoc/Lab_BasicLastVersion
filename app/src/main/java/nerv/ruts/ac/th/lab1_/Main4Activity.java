76a0541a537da48b5f511b9693021654b0e0234c
KnockReservations <witaya.sur@gmail.com>
2018-12-18 00:03:18 +0700

    แก้ไขวันที่ 18/12/61

--- app/src/main/java/nerv/ruts/ac/th/lab1_/Main4Activity.java
+++ app/src/main/java/nerv/ruts/ac/th/lab1_/Main4Activity.java
@@ -22,8 +22,8 @@ public class Main4Activity extends AppCompatActivity {
 
         num1 =(EditText)findViewById(R.id.Num1);
         num2 =(EditText)findViewById(R.id.Num2);
-        OP=(EditText)findViewById(R.id.Op);
-        result=(TextView)findViewById(R.id.result);
+        //OP=(EditText)findViewById(R.id.Op);
+
 
 
         button6= (Button) findViewById(R.id.button6);
@@ -58,15 +58,8 @@ public class Main4Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent start = new Intent(Main4Activity.this,Main5Activity.class);
-                int x = Integer.parseInt(num1.getText().toString());
-                int y = Integer.parseInt(num2.getText().toString());
-                int Plus = x+y;
-                int sub = x-y;
-                int mul = x*y;
-                int divi = x/y;
                 start.putExtra("num1",num1.getText().toString());
                 start.putExtra("num2",num2.getText().toString());
-                start.putExtra("opa",OP.getText().toString());
                 startActivity(start);
             }
         });
--- app/src/main/java/nerv/ruts/ac/th/lab1_/Main5Activity.java
+++ app/src/main/java/nerv/ruts/ac/th/lab1_/Main5Activity.java
@@ -1,28 +1,87 @@
 package nerv.ruts.ac.th.lab1_;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main5Activity extends AppCompatActivity {
-    TextView textnum1,textnum2,textop,result;
+    TextView textnum1,textnum2,textop,Answer;
+    Button Add,Sub,Mul,Div,image1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
-
         textnum1=(TextView)findViewById(R.id.textnum1);
         textnum2=(TextView)findViewById(R.id.textnum2);
-        textop=(TextView)findViewById(R.id.textop);
-        result=(TextView)findViewById(R.id.result);
+        //textop=(TextView)findViewById(R.id.textop);
+        Answer=(TextView)findViewById(R.id.Answer);
+        //
+        Add=(Button)findViewById(R.id.Add);
+        Sub=(Button)findViewById(R.id.Sub);
+        Mul=(Button)findViewById(R.id.Mul);
+        Div=(Button)findViewById(R.id.Div);
 
-
-
+        //
         textnum1.setText(getIntent().getStringExtra("num1"));
         textnum2.setText(getIntent().getStringExtra("num2"));
-        textop.setText(getIntent().getStringExtra("opa"));
-        result.setText(String.valueOf(result));
+        //textop.setText(getIntent().getStringExtra("opa"));
+        //
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(textnum1.getText().toString());
+                int y = Integer.parseInt(textnum2.getText().toString());
+
+                int result = (x+y);
+
+                Answer.setText(String.valueOf(result));
+            }
+        });
+        Sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(textnum1.getText().toString());
+                int y = Integer.parseInt(textnum2.getText().toString());
+
+                int result = (x-y);
+
+                Answer.setText(String.valueOf(result));
+            }
+        });
+        Mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(textnum1.getText().toString());
+                int y = Integer.parseInt(textnum2.getText().toString());
+
+                int result = (x*y);
+
+                Answer.setText(String.valueOf(result));
+            }
+        });
+        Div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(textnum1.getText().toString());
+                int y = Integer.parseInt(textnum2.getText().toString());
+
+                int result = (x/y);
+
+                Answer.setText(String.valueOf(result));
+            }
+        });
+        image1=(Button)findViewById(R.id.btimg);
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        Intent intent = new Intent(Main5Activity.this,MainActivity.class);
+                        finish();
+            }
+        });
 
 
 
